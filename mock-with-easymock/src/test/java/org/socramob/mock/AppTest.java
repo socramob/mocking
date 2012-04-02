0c812d875c214c023d3436b6c64d2a6bba7082cd
Jan Ernsting <jan.ernsting@gmail.com>
2012-04-02 19:06:17 +0200

    Updated to latest JUnit version and added EasyMock

--- mock-with-easymock/src/test/java/org/socramob/mock/AppTest.java
+++ mock-with-easymock/src/test/java/org/socramob/mock/AppTest.java
@@ -1,38 +1,18 @@
 package org.socramob.mock;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void bigTest() {
+		assertThat(true, is(true));
+	}
+	
 }
