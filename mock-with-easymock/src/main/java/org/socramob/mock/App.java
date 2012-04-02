50f39404b767c4c63d2df004edf436b352609d1b
Jan Ernsting <jan.ernsting@gmail.com>
2012-04-02 18:54:05 +0200

    Created structure for sample project

---
+++ mock-with-easymock/src/main/java/org/socramob/mock/App.java
@@ -0,0 +1,13 @@
+package org.socramob.mock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
